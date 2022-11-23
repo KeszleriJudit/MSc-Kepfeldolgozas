86d1d0dbde3f7b391b63e18b3c4fe3a0f85a6527
KeszleriJudit <58305394+KeszleriJudit@users.noreply.github.com>
2022-11-23 22:47:15 +0100

    Adding comments

--- SudokuChecker/SudokuChecker/Functionalities/Implementations/SudokuCheckerImplementation.cs
+++ SudokuChecker/SudokuChecker/Functionalities/Implementations/SudokuCheckerImplementation.cs
@@ -43,20 +43,28 @@ namespace SudokuChecker.Functionalities.Implementations
             this.imageWidth = inputImage.Width;
             this.imageHeight = inputImage.Height;
 
+            /**
+             * Doing some pre image processing
+             */
             Bitmap greyscaledImage = greyScaling(inputImage);
             Bitmap gausBlurredImage = gausBlur(greyscaledImage);
             Bitmap contrastStrechedImage = contrastStreching(gausBlurredImage);
-            //körbevágás sarokpont + éldetektor
-
+            
             string name = $"C:/Users/Judit/Desktop/SudokuSolver/{this.counter}-sudoku.png";
             contrastStrechedImage.Save(name, ImageFormat.Png);
 
+            /**
+             * Calling the python script to detect and return with the relevant image part
+             */
             RunPythonScript("contours.py", this.counter.ToString());
 
             string processedImagePath = $"C:/Users/Judit/Desktop/SudokuSolver/{this.counter}-sudoku-processed.png";
             Bitmap processedImage = new Bitmap(processedImagePath);
             this.counter++;
 
+            /**
+             * Cutting the image into smaller sudoku units
+             */
             int processedImageHeight = processedImage.Height;
             int processedImageWidth = processedImage.Width;
             double heightValue = processedImageHeight / 9;
@@ -96,7 +104,6 @@ namespace SudokuChecker.Functionalities.Implementations
             /**
              * Code below is for the processed image to read the numbers
              */
-
             var Ocr = new IronTesseract();
             List<string> sudokuInput = new List<string>();
             Ocr.Configuration.PageSegmentationMode = TesseractPageSegmentationMode.SingleChar;
@@ -108,7 +115,6 @@ namespace SudokuChecker.Functionalities.Implementations
             {
                 var Input = new OcrInput(units[i]);
                 Input.Deskew();
-                Input.ToGrayScale();
                 Input.Invert();
                 Input.Binarize();
                 Input.SaveAsImages($"C:/Users/Judit/Desktop/SudokuSolver/AfterOcr/afterOcr{i}");
@@ -133,6 +139,9 @@ namespace SudokuChecker.Functionalities.Implementations
 
             }
 
+            /**
+             * Log the values read from the image
+             */
             int counter = 0;
             string line = "";
             string sudokuTable = "";
@@ -153,6 +162,9 @@ namespace SudokuChecker.Functionalities.Implementations
                 }
             }
 
+            /**
+             * Call the sudoku solver python script
+             */
             RunPythonScript("solver.py", sudokuTable);
             Console.WriteLine("Finished");
 
@@ -217,21 +229,14 @@ namespace SudokuChecker.Functionalities.Implementations
             Rectangle rectangle = new Rectangle(0, 0, this.imageWidth, this.imageHeight);
             int blurSize = 1;
 
-            // make an exact copy of the bitmap provided
             using (Graphics graphics = Graphics.FromImage(newImage))
                 graphics.DrawImage(inputImage, new Rectangle(0, 0, this.imageWidth, this.imageHeight),
                     new Rectangle(0, 0, this.imageWidth, this.imageHeight), GraphicsUnit.Pixel);
 
-            // Lock the bitmap's bits
             BitmapData blurredData = newImage.LockBits(new Rectangle(0, 0, this.imageWidth, this.imageHeight), ImageLockMode.ReadWrite, newImage.PixelFormat);
-
-            // Get bits per pixel for current PixelFormat
             int bitsPerPixel = Image.GetPixelFormatSize(newImage.PixelFormat);
-
-            // Get pointer to first line
             byte* scan0 = (byte*)blurredData.Scan0.ToPointer();
 
-            // look at every pixel in the blur rectangle
             for (int xx = rectangle.X; xx < rectangle.X + rectangle.Width; xx++)
             {
                 for (int yy = rectangle.Y; yy < rectangle.Y + rectangle.Height; yy++)
@@ -239,19 +244,14 @@ namespace SudokuChecker.Functionalities.Implementations
                     int avgR = 0, avgG = 0, avgB = 0;
                     int blurPixelCount = 0;
 
-                    // average the color of the red, green and blue for each pixel in the
-                    // blur size while making sure you don't go outside the image bounds
                     for (int x = xx; (x < xx + blurSize && x < this.imageWidth); x++)
                     {
                         for (int y = yy; (y < yy + blurSize && y < this.imageHeight); y++)
                         {
-                            // Get pointer to RGB
                             byte* data = scan0 + y * blurredData.Stride + x * bitsPerPixel / 8;
-
-                            avgB += data[0]; // Blue
-                            avgG += data[1]; // Green
-                            avgR += data[2]; // Red
-
+                            avgB += data[0];
+                            avgG += data[1];
+                            avgR += data[2];
                             blurPixelCount++;
                         }
                     }
@@ -260,15 +260,11 @@ namespace SudokuChecker.Functionalities.Implementations
                     avgG = avgG / blurPixelCount;
                     avgB = avgB / blurPixelCount;
 
-                    // now that we know the average for the blur size, set each pixel to that color
                     for (int x = xx; x < xx + blurSize && x < this.imageWidth && x < rectangle.Width; x++)
                     {
                         for (int y = yy; y < yy + blurSize && y < this.imageHeight && y < rectangle.Height; y++)
                         {
-                            // Get pointer to RGB
                             byte* data = scan0 + y * blurredData.Stride + x * bitsPerPixel / 8;
-
-                            // Change values
                             data[0] = (byte)avgB;
                             data[1] = (byte)avgG;
                             data[2] = (byte)avgR;
@@ -276,10 +272,7 @@ namespace SudokuChecker.Functionalities.Implementations
                     }
                 }
             }
-
-            // Unlock the bits
             newImage.UnlockBits(blurredData);
-
             return newImage;
         }
 
@@ -288,31 +281,24 @@ namespace SudokuChecker.Functionalities.Implementations
             double blackPointPercent = 0.05;
             double whitePointPercent = 0.2;
             Bitmap newImage = new Bitmap(this.imageWidth, this.imageHeight);
-            //Lock bits for your source image into system memory
+
             Rectangle rect = new Rectangle(0, 0, this.imageWidth, this.imageHeight);
             BitmapData srcData = inputImage.LockBits(rect, ImageLockMode.ReadOnly,
                 PixelFormat.Format32bppArgb);
 
-            //Lock bits for your writable bitmap into system memory
+
             Rectangle rect2 = new Rectangle(0, 0, newImage.Width, newImage.Height);
             BitmapData destData = newImage.LockBits(rect2, ImageLockMode.WriteOnly,
                 PixelFormat.Format32bppArgb);
 
-            //Get the width of a single row of pixels in the bitmap
             int stride = srcData.Stride;
-
-            //Scan for the first pixel data in bitmaps
             IntPtr srcScan0 = srcData.Scan0;
             IntPtr destScan0 = destData.Scan0;
-
             var freq = new int[256];
 
             unsafe
             {
-                //Create an array of pixel data from source image
                 byte* src = (byte*)srcScan0;
-
-                //Get the number of pixels for each intensity value
                 for (int y = 0; y < newImage.Height; ++y)
                 {
                     for (int x = 0; x < this.imageWidth; ++x)
@@ -320,60 +306,33 @@ namespace SudokuChecker.Functionalities.Implementations
                         freq[src[y * stride + x * 4]]++;
                     }
                 }
-
-                //Get the total number of pixels in the image
                 int numPixels = this.imageWidth * this.imageHeight;
-
-                //Set the minimum intensity value of an image (0 = black)
                 int minI = 0;
-
-                //Get the total number of black pixels
                 var blackPixels = numPixels * blackPointPercent;
-
-                //Set a variable for summing up the pixels that will turn black
                 int accum = 0;
-
-                //Sum up the darkest shades until you reach the total of black pixels
                 while (minI < 255)
                 {
                     accum += freq[minI];
                     if (accum > blackPixels) break;
                     minI++;
                 }
-
-                //Set the maximum intensity of an image (255 = white)
                 int maxI = 255;
-
-                //Set the total number of white pixels
                 var whitePixels = numPixels * whitePointPercent;
-
-                //Reset the summing variable back to 0
                 accum = 0;
-
-                //Sum up the pixels that are the lightest which will turn white
                 while (maxI > 0)
                 {
                     accum += freq[maxI];
                     if (accum > whitePixels) break;
                     maxI--;
                 }
-
-                //Part of a normalization equation that doesn't vary with each pixel
                 double spread = 255d / (maxI - minI);
-
-                //Create an array of pixel data from created image
                 byte* dst = (byte*)destScan0;
-
-                //Write new pixel data to the image
                 for (int y = 0; y < this.imageHeight; ++y)
                 {
                     for (int x = 0; x < this.imageWidth; ++x)
                     {
                         int i = y * stride + x * 4;
-
-                        //Part of equation that varies with each pixel
                         double value = Math.Round((src[i] - minI) * spread);
-
                         byte val = (byte)(Math.Min(Math.Max(value, 0), 255));
                         dst[i] = val;
                         dst[i + 1] = val;
@@ -382,11 +341,8 @@ namespace SudokuChecker.Functionalities.Implementations
                     }
                 }
             }
-
-            //Unlock bits from system memory
             inputImage.UnlockBits(srcData);
             newImage.UnlockBits(destData);
-
             return newImage;
         }
 
