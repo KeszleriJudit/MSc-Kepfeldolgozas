cc395b3725184abee740e6f462f9621800dbd626
KeszleriJudit <58305394+KeszleriJudit@users.noreply.github.com>
2022-11-24 22:52:08 +0100

    Remove unnecessary code

--- SudokuChecker/SudokuChecker/Functionalities/ProgramFunction.cs
+++ SudokuChecker/SudokuChecker/Functionalities/ProgramFunction.cs
@@ -12,13 +12,6 @@ namespace SudokuChecker.Functionalities
         Ps_Negalas,
         Ps_Gamma_Transzformacio,
         Ps_Logaritmikus_Transzformacio,
-        Ps_Szurkites,
-        Ps_Hisztogram_Keszites,
-        Ps_Hisztogram_Kiegyenlites,
-        Ps_Atlagolo_Szuro,
-        Ps_Gauss_Szuro,
-        Ps_Sobel_Eldetektor,
-        Ps_Laplace_Eldetektor,
-        Ps_Jellemzo_Pontok_Detektalasa
+        Ps_Szurkites
     }
 }
